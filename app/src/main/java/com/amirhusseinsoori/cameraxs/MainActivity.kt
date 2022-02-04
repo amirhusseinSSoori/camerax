3361192de7850572839cda5cb00d13dae4658249
Amirhussein soori <amirhusseinsppro75@gmail.com>
2022-02-04 12:26:29 +0330

    add CameraFragment instead of CameraActivity

--- app/src/main/java/com/amirhusseinsoori/cameraxs/MainActivity.kt
+++ app/src/main/java/com/amirhusseinsoori/cameraxs/MainActivity.kt
@@ -11,29 +11,10 @@ import androidx.core.content.ContextCompat
 import com.amirhusseinsoori.cameraxs.camera.CameraActivity
 import com.amirhusseinsoori.cameraxs.camera.VideoActivity
 import com.amirhusseinsoori.cameraxs.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-
-    private val requestPermissionLauncherCamera =
-        registerForActivityResult(RequestPermission()) { isGranted ->
-            if (isGranted) {
-                startActivity(Intent(this, CameraActivity::class.java))
-
-            } else {
-                checkForPermissions()
-            }
-        }
-
-    private val requestPermissionLauncherVideo =
-        registerForActivityResult(RequestPermission()) { isGranted ->
-            if (isGranted) {
-                startActivity(Intent(this, VideoActivity::class.java))
-
-            } else {
-                checkForPermissions()
-            }
-        }
-
 
     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,41 +22,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnCamera.setOnClickListener {
-            requestPermissionLauncherCamera.launch(android.Manifest.permission.CAMERA)
-        }
-        binding.btnVideo.setOnClickListener {
-            requestPermissionLauncherVideo.launch(android.Manifest.permission.CAMERA)
-        }
     }
 
-
-    private fun checkForPermissions(): Boolean {
-        if (ActivityCompat.checkSelfPermission(
-                applicationContext,
-                Manifest.permission.CAMERA
-            ) != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(
-                applicationContext,
-                Manifest.permission.READ_EXTERNAL_STORAGE
-            )
-            != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(
-                applicationContext,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE
-            )
-            != PackageManager.PERMISSION_GRANTED
-
-        ) {
-            ActivityCompat.requestPermissions(
-                this,
-                listOf(
-                    Manifest.permission.CAMERA,
-                    Manifest.permission.READ_EXTERNAL_STORAGE,
-                    Manifest.permission.WRITE_EXTERNAL_STORAGE
-                ).toTypedArray(), 10005
-            )
-            return false
-        }
-        return true
-    }
 
 }
---
+++ app/src/main/java/com/amirhusseinsoori/cameraxs/ui/StartFragment.kt
@@ -0,0 +1,82 @@
+package com.amirhusseinsoori.cameraxs.ui
+
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.view.View
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.amirhusseinsoori.cameraxs.R
+import com.amirhusseinsoori.cameraxs.camera.CameraActivity
+import com.amirhusseinsoori.cameraxs.camera.VideoActivity
+import com.amirhusseinsoori.cameraxs.databinding.FragmentStartBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class StartFragment : Fragment(R.layout.fragment_start) {
+    lateinit var bind:FragmentStartBinding
+    private val requestPermissionLauncherCamera =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if (isGranted) {
+               //startActivity(Intent(activity, CameraActivity::class.java))
+                findNavController().navigate(R.id.action_startFragment_to_cameraFragment)
+            } else {
+                checkForPermissions()
+            }
+        }
+//
+    private val requestPermissionLauncherVideo =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if (isGranted) {
+                startActivity(Intent(activity, VideoActivity::class.java))
+
+            } else {
+                checkForPermissions()
+            }
+        }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        bind=FragmentStartBinding.bind(view)
+        super.onViewCreated(view, savedInstanceState)
+        bind.btnCamera.setOnClickListener {
+            requestPermissionLauncherCamera.launch(android.Manifest.permission.CAMERA)
+        }
+        bind.btnVideo.setOnClickListener {
+            requestPermissionLauncherVideo.launch(android.Manifest.permission.CAMERA)
+        }
+    }
+
+
+
+    private fun checkForPermissions(): Boolean {
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.CAMERA
+            ) != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            )
+            != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            )
+            != PackageManager.PERMISSION_GRANTED
+
+        ) {
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                listOf(
+                    Manifest.permission.CAMERA,
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                ).toTypedArray(), 10005
+            )
+            return false
+        }
+        return true
+    }
+
+}
